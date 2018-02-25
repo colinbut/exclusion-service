8595174f0a2ef17ea8824375100f5d52fc3f4951
Colin But <colinbut@users.noreply.github.com>
2018-02-25 09:32:19 +0000

    Deployment setup - finalized the install script bash

--- install.sh
+++ install.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
+############################################################################
+#
+# Author: Colin But - colin.but@outlook.com
+#
+# Bash Script to automate the creation of deployment artefacts
+#
+# version 1.0
+#
+############################################################################
+
 baseDir=$PWD
+exclusionServiceArtefact=exclusion-service.war
+dockerImageTag=exclusion-service:1.0-SNAPSHOT
+dockerImageArtefact=exclusion-service-1.0-SNAPSHOT.tar.gz
 
 cd docker;
-if [ -f exclusion-service.war ]; then
-    echo 'Removing existing exclusion-service.war artefact';
-    rm -v exclusion-service.war;
+if [ -f ${dockerImageArtefact} ]; then
+    echo "Removing existing ${dockerImageArtefact} artefact";
+    rm -v ${dockerImageArtefact};
 else
-    echo 'No existing exclusion-service.war exists - proceed as normal';
+    echo "No existing ${dockerImageArtefact} exists - proceed as normal";
 fi
 
-if [ ! -f ${baseDir}/target/exclusion-service.war ]; then
-    echo 'exclusion-service.war artefact does not exist - creating it';
+if [ ! -f ${baseDir}/target/${exclusionServiceArtefact} ]; then
+    echo "${exclusionServiceArtefact} artefact does not exist - creating it";
     cd ${baseDir};
     mvn clean install
     cd -
 fi
 
-echo 'Copying built artefact exclusion-service.war to deployment directory';
-cp -v ${baseDir}/target/exclusion-service.war .
+echo "Copying built artefact ${exclusionServiceArtefact} to deployment directory";
+cp -v ${baseDir}/target/${exclusionServiceArtefact} .
 
 
-docker build -t exclusion-service:1.0-SNAPSHOT .
+docker build -t ${dockerImageTag} .
+
+docker save -o ${dockerImageArtefact} ${dockerImageTag}
+
+echo "Removing ${exclusionServiceArtefact} artefact";
+rm -v ${exclusionServiceArtefact};
 
 
 
